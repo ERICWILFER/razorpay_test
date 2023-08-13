54c56ee8e115d6134b70d4ebf279a28509c63441
Eric Wilfer <73844250+ERICWILFER@users.noreply.github.com>
2023-08-13 12:07:47 +0530

    course type and mobile variable added

--- src/App.jsx
+++ src/App.jsx
@@ -1,121 +1,122 @@
-import './App.css'
+import "./App.css";
 import axios from "axios";
 
 const razorPayKeyId = "rzp_test_8nN58e8ffLtkXT";
-const accessToken = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJUSEVfSVNTVUVSIiwiYXVkIjoiVEhFX0FVRElFTkNFIiwiaWF0IjoxNjkxNTc3NTUzLCJuYmYiOjE2OTE1Nzc1NTMsImV4cCI6MTY5MTU4MTE1MywiZGF0YSI6eyJ1c2VyaWQiOiIwIiwicmVmaWQiOiJTVFVEMzI5ODQ2IiwidXNlcnR5cGUiOiJzdXBlcl9hZG1pbiIsInVzZXJuYW1lIjoic3VwZXJfYWRtaW4iLCJtb2JpbGUiOiI1NTU1NTU1NTU1Iiwicm9sZSI6InN1cGVyX2FkbWluIiwic2Nob29saWQiOiJBTFBINTEyNzgwIn19.c0YA3-R_Y48ait7CXJyWKzE-qUxJ8elHYbg5sQ2w5Rw"
+const accessToken =
+  "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJUSEVfSVNTVUVSIiwiYXVkIjoiVEhFX0FVRElFTkNFIiwiaWF0IjoxNjkxODYzNDYxLCJuYmYiOjE2OTE4NjM0NjEsImV4cCI6MTY5MTg2NzA2MSwiZGF0YSI6eyJ1c2VyaWQiOiIwIiwicmVmaWQiOiJBTFBINTEyNzgwIiwidXNlcnR5cGUiOiJzY2hvb2wiLCJ1c2VybmFtZSI6ImhlYWRtYXN0ZXIgcHJlbSIsIm1vYmlsZSI6Ijg5MzQyMTYzNDIiLCJyb2xlIjoidXNlciIsInNjaG9vbGlkIjoiQUxQSDUxMjc4MCJ9fQ.T-9d8OSi4otBf91d5zMWUxIfgmL7_L0Xphk2ybrQL6c";
 const courseid = 7;
-const coursetype = "advanced";
+const coursetype = "beginner";
+const mobile = "8934216342" // school login mobile
 
 export default function App() {
-      return <>
-          <CoursePayment />
-          {/* <ProductPayment /> */}
-          </>
-      
-  }
+  return (
+    <>
+      <CoursePayment />
+      {/* <ProductPayment /> */}
+    </>
+  );
+}
 
-  const CoursePayment = () => {
-    const handlePayment = async (data) => {
-      console.log("handlePayment data:", data);
-      try {
-        const response = await axios.post(
-          "http://localhost/multeartsapi/api/payment/verify-course-payment.php",
-          data,
-          {
-            headers: {
-              Authorization: accessToken
-            }
-          }
-        );
-  
-        console.log("response", response);
-  
-      }
-      catch(error) {
-        console.log("error", error);
-      }
-    }
-    
-    const initPayment = (data) => {
-      console.log("initPayment  data:", data);
-  
-      const options = {
-        key: razorPayKeyId, // Enter the Key ID generated from the Dashboard
-        amount: data.amount, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
-        currency: "INR",
-        name: "Eric",
-        description: "Test Transaction",
-        // "image": "https://example.com/your_logo",
-        order_id: data.orderId, //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
-        handler: function (response) {
-          const data = {
-              razorpay_payment_id: response.razorpay_payment_id,
-              razorpay_order_id: response.razorpay_order_id,
-              razorpay_signature: response.razorpay_signature,
-              keyid: razorPayKeyId,
-              courseid: courseid,
-          }
-          handlePayment(data)
-        },
-        "prefill": {
-            "name": "Eric",
-            "contact": "5555555555",
-        },
-        "notes": {
-            "address": "Razorpay Corporate Office", 
-            "mobile": "5555555555",
-            "courseid": courseid,
-            "coursetype": coursetype,
-            "paymenttype": "course",
-        },
-        "theme": {
-            "color": "#3399cc",
+const CoursePayment = () => {
+  const handlePayment = async (data) => {
+    console.log("handlePayment data:", data);
+    try {
+      const response = await axios.post(
+        "http://localhost/multeartsapi/api/payment/verify-course-payment.php",
+        data,
+        {
+          headers: {
+            Authorization: accessToken,
+          },
         }
-      };
-  
-      const rPI = new window.Razorpay(options);
-  
-      rPI.open();
+      );
+
+      console.log("response", response);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+  const initPayment = (data) => {
+    console.log("initPayment  data:", data);
+
+    const options = {
+      key: razorPayKeyId, // Enter the Key ID generated from the Dashboard
+      amount: data.amount, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+      currency: "INR",
+      name: "Eric",
+      description: "Test Transaction",
+      // "image": "https://example.com/your_logo",
+      order_id: data.orderId, //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
+      handler: function (response) {
+        const data = {
+          razorpay_payment_id: response.razorpay_payment_id,
+          razorpay_order_id: response.razorpay_order_id,
+          razorpay_signature: response.razorpay_signature,
+          keyid: razorPayKeyId,
+          courseid: courseid,
+        };
+        handlePayment(data);
+      },
+      prefill: {
+        name: "Eric",
+        contact: mobile,
+      },
+      notes: {
+        address: "Razorpay Corporate Office",
+        mobile: mobile,
+        courseid: courseid,
+        coursetype: coursetype,
+        paymenttype: "course",
+      },
+      theme: {
+        color: "#3399cc",
+      },
     };
-  
-    const createOrder = async (data) => {
-      try {
-        const response = await axios.post(
-          "http://localhost/multeartsapi/api/payment/create-course-payment.php",
-          data,
-          {
-            headers: {
-            Authorization: accessToken
-            }
-          }
-        );
-  
-        console.log("response", response);
-  
-        initPayment(response?.data?.data);
-      } catch (error) {
-        console.log("error", error);
-      }
+
+    const rPI = new window.Razorpay(options);
+
+    rPI.open();
+  };
+
+  const createOrder = async (data) => {
+    try {
+      const response = await axios.post(
+        "http://localhost/multeartsapi/api/payment/create-course-payment.php",
+        data,
+        {
+          headers: {
+            Authorization: accessToken,
+          },
+        }
+      );
+
+      console.log("response", response);
+
+      initPayment(response?.data?.data);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+  const createPayment = () => {
+    const data = {
+      keyid: razorPayKeyId,
+      courseid,
+      coursetype,
     };
-  
-    const createPayment = () => {
-      const data = {
-        keyid: razorPayKeyId,
-        courseid,
-        coursetype
-      };
-  
-      createOrder(data);
-    };
-  
-    return (
-      <div className="App">
-        <h1>Razorpay Test</h1>
-        <button onClick={createPayment}>Pay</button>
-      </div>
-    );
-  }
-  
+
+    createOrder(data);
+  };
+
+  return (
+    <div className="App">
+      <h1>Razorpay Test</h1>
+      <button onClick={createPayment}>Pay</button>
+    </div>
+  );
+};
+
 // const ProductPayment = () => {
 //   const handlePayment = async (data) => {
 //     console.log("handlePayment data:", data);
@@ -129,18 +130,18 @@ export default function App() {
 //           }
 //         }
 //       );
-// 
+//
 //       console.log("response", response);
-// 
+//
 //     }
 //     catch(error) {
 //       console.log("error", error);
 //     }
 //   }
-//   
+//
 //   const initPayment = (data) => {
 //     console.log("initPayment  data:", data);
-// 
+//
 //     const options = {
 //       key: razorPayKeyId, // Enter the Key ID generated from the Dashboard
 //       amount: data.amount, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
@@ -161,11 +162,11 @@ export default function App() {
 //       },
 //       "prefill": {
 //           "name": "Eric",
-//           "contact": "5555555555",
+//           "contact": mobile,
 //       },
 //       "notes": {
-//           "address": "Razorpay Corporate Office", 
-//           "mobile": "5555555555",
+//           "address": "Razorpay Corporate Office",
+//           "mobile": mobile,
 //           "courseid": courseid,
 //           "coursetype": coursetype,
 //           "paymenttype": "products",
@@ -174,12 +175,12 @@ export default function App() {
 //           "color": "#3399cc",
 //       }
 //     };
-// 
+//
 //     const rPI = new window.Razorpay(options);
-// 
+//
 //     rPI.open();
 //   };
-// 
+//
 //   const createOrder = async (data) => {
 //     try {
 //       const response = await axios.post(
@@ -191,23 +192,23 @@ export default function App() {
 //           }
 //         }
 //       );
-// 
+//
 //       console.log("response", response);
-// 
+//
 //       initPayment(response?.data?.data);
 //     } catch (error) {
 //       console.log("error", error);
 //     }
 //   };
-// 
+//
 //   const createPayment = () => {
 //     const data = {
 //       keyid: razorPayKeyId,
 //     };
-// 
+//
 //     createOrder(data);
 //   };
-// 
+//
 //   return (
 //     <div className="App">
 //       <h1>Razorpay Test</h1>
@@ -215,4 +216,3 @@ export default function App() {
 //     </div>
 //   );
 // }
-
